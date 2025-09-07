ee7a684f887091ba4f12456fad633c9fdc59c17f
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-09-07 20:13:38 -0300

    feat(duelo): implementa funções comentadas para o duelo entre personagens e modal
    - Adiciona lógica para duelo comparando níveis dos personagens
    - Implanta modal para interação visual do duelo

--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -15,6 +15,17 @@ export class personagem {
     }
     return `${this.constructor.tipo} iniciante `;
   }
+  static verificarVencedor(personagem1, personagem2) {
+    if (personagem1.level === personagem2.level) {
+      return "Empate!!!";
+    }
+
+    if (personagem1.level > personagem2.level) {
+      return `${personagem1.constructor.tipo} ${personagem1.nome} é o vencedor!`;
+    }
+
+    return `${personagem2.constructor.tipo} ${personagem2.nome} é o vencedor!`;
+  }
 }
 export class mago extends personagem {
   static tipo = "Mago";
